6425dfc00e3a2191a84e88f4e5453ed01d2e6838
Professor Marcelo <avalia.apb2023@gmail.com>
2024-04-17 15:32:39 -0300

    subir estagio 2 da api

--- api.js
+++ api.js
@@ -1,24 +1,21 @@
-//requires
+// 17/04  criar dois novos endpoints
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
-//imports
 const mysql_config = require('./imp/mysql_config');
 const functions = require('./imp/functions');
 const { truncateSync } = require('fs');
 
-//variáveis para disponibilidade e para versionamento
 const API_AVAILABILITY = true;
 const API_VERSION = '1.0.0';
 
-//iniciar servidor
+
 const app = express();
 app.listen(3000,()=>{
     console.log("API está executando");
 })
 
-//verificar a disponbibilidade da API
 app.use((req,res,next)=>{
     if(API_AVAILABILITY){
         next()
@@ -28,10 +25,9 @@ app.use((req,res,next)=>{
     }
 })
 
-//conexão com mysql
+
 const connection = mysql.createConnection(mysql_config);
 
-//cors
 app.use(cors());
 
 //rotas 
@@ -53,7 +49,42 @@ app.get('/tasks',(req,res)=>{
     })
 })
 
-//tratar o erro de rota
+
+//rota para fazer uma consulta de task por id
+app.get('/tasks/:id',(req,res)=>{
+    const id =req.params.id;
+    connection.query('SELECT * FROM tasks WHERE id = ?',[id],(err,rows)=>{
+        if(!err){
+            if(rows.length>0){
+                res.json(functions.response('Sucesso','Sucesso na pesquisa',rows.length,rows))
+            }else{
+                res.json(functions.response('Atenção','Não foi encontrada a task selecionada',0,null))
+            }   
+        }else{
+            res.json(functions.response('erro',err.message,0,null))
+        }
+    })
+})
+
+//rota para atualizar o status da task pelo id selecionado
+app.put('/tasks/:id/status/:status',(req,res)=>{
+    const id = req.params.id;
+    const status = req.params.status;
+    connection.query('UPDATE tasks SET status =? WHERE id = ?',[status,id],(err,rows)=>{
+        if(!err){
+            if(rows.affectedRows>0){
+                res.json(functions.response('Sucesso','Sucesso na alteração do status',rows.affectedRows,null))
+            }else{
+                res.json(functions.response('Alerta vermelho','Task não encontradad',0,null))
+            }
+        }else{
+            res.json(functions.response('Erro',err.message,0,null))
+        }
+    })
+})
+
+
+
 app.use((req,res)=>{
     res.json(functions.response('atenção',
                 'Rota não encontrada',0,null))
